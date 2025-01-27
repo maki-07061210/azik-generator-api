7d8b7b49680a38e50fae009cafa351d6c7493769
maki <maki111000664455@gmail.com>
2025-01-27 11:04:48 +0900

    binクレートはもしかして外部依存としてインポート出来ないのか？

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-use axum::{routing::get, Router};
+mod generate_kana_table;
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
-}
+use generate_kana_table::azik_config::AzikConfig;
+use generate_kana_table::gen_kana_table::gen_hiragana_table;
+
+pub use crate::generate_kana_table::gen_kana_table;
+use axum::{http::StatusCode, routing::post, Json, Router};
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new().route("/", post(gen_kana_text));
 
     Ok(router.into())
+}
+
+async fn gen_kana_text(Json(payload): Json<AzikConfig>) -> (StatusCode, String) {
+    let azik = gen_hiragana_table(payload);
+    (StatusCode::OK, azik)
 }
